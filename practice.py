86d2131e8b9e1171c6bd0cd60a48f219cedee20a
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2025-11-27 17:46:42 +0100

    practice.py

---
+++ practice.py
@@ -0,0 +1,41 @@
+# store word in python dictionarry
+dictionary={
+"cat": "a small animal",
+"table " : ["a pice of furnitue ", "List of facts and figures"]
+
+
+
+}
+
+print(dictionary)
+
+
+#total classroom
+
+
+list=["Python","c++","Java", "Javascript"]
+print("Total classroom number:",len(list))
+
+
+#new dictionary
+students={}
+students["che"] = 98
+students["Phy"]=85
+students["bio"]=95
+
+print(students)
+
+
+#set
+values={9, "9.0"}
+print(values)
+
+#with tuples
+v= {
+
+    ("int", 9),
+    ("string", 9.0)
+}
+print(v)
+
+
