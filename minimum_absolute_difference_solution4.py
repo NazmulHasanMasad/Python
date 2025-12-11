77a99add6be01b34d1871f0fb5ae8880bf3fc33d
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2025-12-11 20:52:52 +0100

    1200_solution4

---
+++ minimum_absolute_difference_solution4.py
@@ -0,0 +1,8 @@
+
+from itertools import pairwise
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        mn = min(b - a for a, b in pairwise(arr))
+        return [[a, b] for a, b in pairwise(arr) if b-a==mn]
