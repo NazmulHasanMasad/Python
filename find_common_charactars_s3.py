3cb8cd7e59829ca23380448298070f67dc29cc69
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2025-12-14 04:13:14 +0100

    1002_solution3

---
+++ find_common_charactars_s3.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+
+class Solution:
+
+    def commonChars(self, words: List[str]) -> List[str]:
+        min_freq= Counter(words[0])
+        for word in words:
+            min_freq &= Counter(word)
+
+        return list(min_freq.elements())
